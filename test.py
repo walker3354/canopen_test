c5a2daf1d14dff4c048a318e6a85733c80d6b16f
walker3354 <walker3354827@gmail.com>
2023-06-17 14:01:53 +0800

    version 1.1

--- test.py
+++ test.py
@@ -10,8 +10,8 @@ class CsCanOpen:
     def __init__(self, can_interface, config_file):
         self.porx_node_list = []
         self.light_node_list = []
-        self.control_id
-        self.pre_control_id
+        self.control_id = 0
+        self.pre_control_id = 0
         self.config = self.load_config(config_file)
         self.can_network = self.canopen_init(can_interface)
         self.load_light_nodes(self.config["light"])
@@ -47,7 +47,7 @@ class CsCanOpen:
         print("Check all prox nodes completed!")
         for node in self.porx_node_list:
             node.tpdo.read()
-            self.control_id = self.cnode.tpdo[1].add_callback(
+            self.control_id = node.tpdo[1].add_callback(
                 self.proximity_callback)
             if self.control_id != 0:
                 for light_node in self.light_node_list:
@@ -71,8 +71,8 @@ class CsCanOpen:
     def proximity_callback(self, msg):
         node_id = msg.cob_id - 384
         for var in msg:
-            self.gesture.update_prox(node_id, var.raw)
-            if var.raw < 50:
+            ##self.gesture.update_prox(node_id, var.raw)
+            if var.raw < 40:
                 return node_id
 
     def disconnect(self):
@@ -84,23 +84,6 @@ def main():
     while True:
         time.sleep(1)
     return
-    '''
-    light_network = canopen.Network()
-    light_network.connect(channel="can0", bustype='socketcan')
-    node = light_network.add_node(
-        25, 'DS301_profile_mcu.eds')
-    node.rpdo.read()
-    node.nmt.state = 'PRE-OPERATIONAL'
-    # print(node.rpdo.len())
-    node.rpdo[1][0x6001].phys = 0x01
-    node.rpdo[1].start(0.1)
-    node.nmt.state = 'OPERATIONAL'
-    time.sleep(2)
-    node.rpdo[1][0x6001].phys = 0x00
-    time.sleep(2)
-    node.rpdo[1].stop()
-    return
-    '''
 
 
 if __name__ == "__main__":
